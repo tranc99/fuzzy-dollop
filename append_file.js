1870fbd1b7197603e7a3e181db846238723f651b
Tendai Mutunhire <tmutunhire@gmail.com>
2017-07-24 21:35:56 +0200

    add file changes

--- append_file.js
+++ append_file.js
@@ -2,3 +2,9 @@
 
 //imports the fs module
 const fs = require('fs');
+
+// add a line to a lyric file, using appendFile
+fs.appendFile('empirestate.txt', 'Right there up on Broadway', (err) => {
+  if (err) throw err;
+  console.log('The lyrics were updated!');
+});
